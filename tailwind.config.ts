fc306875f400ff5a3d5859f18346e5cf7479b462
Mihir Sahu <0xmihirsahu@gmail.com>
2024-11-16 03:18:04 +0700

    tailwindcss fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,10 +6,12 @@ const {
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
 	],
   prefix: "",
   theme: {
